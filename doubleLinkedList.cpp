a4a6f9702fe74af4715783f3b752c927defb5de3
NurulNajatMony <nurulmony505@gmail.com>
2025-05-15 12:28:32 +0700

    menambahkan validasi duplikat saat insert di posisi awal

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -36,4 +36,10 @@ class DoubleLinkedList
 
          //Step 3: Insert at beginning if list is empty or nim is smallest
          if (START == NULL || nim <= START ->noMhs)
-        
+         {
+             if (START != NULL && nim == START->noMhs)
+             {
+                 cout << "\nDuplicate number not allowed" << endl;
+                 return;
+             }
+             
