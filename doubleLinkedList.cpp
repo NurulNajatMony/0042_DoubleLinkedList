d443a56d1d50dbce5f6d8e776af4a5b8b2ffd336
NurulNajatMony <nurulmony505@gmail.com>
2025-05-15 12:40:15 +0700

    menambahkan validasi duplikat saat insert di tengah list

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -62,3 +62,10 @@ class DoubleLinkedList
             {
                 current = current->next;
             }
+
+            if (current->next !=NULL && nim == current->next->noMhs)
+            {
+                cout << "\nDuplicate roll number not allowed" << endl;
+                return;
+            }
+
